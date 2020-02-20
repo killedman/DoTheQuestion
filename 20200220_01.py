efe9a235c71a81d965b1054c65c8d7529067775a
killedman <killedmanbylonely@gmail.com>
2020-02-20 08:41:27 +0800

    题目:best-time-to-buy-and-sell-stock，买卖股票的最佳时机【备注：列表非常长时会超过leetcode的时间】

---
+++ 20200220_01.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+"""
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+
+注意你不能在买入股票前卖出股票。
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+
+示例 2:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit_list = []
+        while prices:
+            temp = prices[0]
+            prices = prices[1:]
+            profit_list.extend([price - temp for price in prices if price > temp])
+        # print(profit_list)
+        if len(profit_list) > 0:
+            return max(profit_list)
+        else:
+            return 0
+
+
+if __name__ == "__main__":
+    # 当列表非常长时，在leetcode上提交会出现超时问题
+    ss = [7,6,4,3,1]
+    solution = Solution()
+    result = solution.maxProfit(ss)
+    print(result)
